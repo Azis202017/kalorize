ff2b435153933a19e80cc9f29207477120c63cad
Azis202017 <azisa6980@gmail.com>
2024-01-01 15:38:05 +0700

    [Service] added Auth Service

--- lib/app/services/auth_service.dart
+++ lib/app/services/auth_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:kalorize/app/constant/global.dart';
 import 'package:kalorize/app/services/input/login_input.dart';
+import 'package:kalorize/app/services/input/register_input.dart';
 
 class AuthService {
   Future<bool> login(LoginInput loginInput) async {
@@ -10,6 +11,31 @@ class AuthService {
       Map<String, dynamic> body = {
         "email": loginInput.email,
         "password": loginInput.password,
+      };
+      final response = await http.post(
+        url,
+        body: jsonEncode(body),
+        headers: headers,
+      );
+      if (response.statusCode == 200) {
+        return true;
+      }
+      return false;
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Future<bool> register(RegisterInput registerInput) async {
+    try {
+      Uri url = Uri.parse('$apiUrl/register');
+      Map<String, dynamic> body = {
+        "NamaLengkap" : registerInput.nama,
+        "Email": registerInput.email,
+        "Password": registerInput.password,
+        "PasswordConfirmation" : registerInput.passwordConfirmation,
+        "kodeGym" : registerInput.kodeGym,
+        "kodeReferal" : registerInput.kodeReferral
       };
       final response = await http.post(
         url,
@@ -25,4 +51,5 @@ class AuthService {
       throw Exception(e);
     }
   }
+  
 }
