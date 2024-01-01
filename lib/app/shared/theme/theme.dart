2daf29572e91cab826e6023a950dfcde0d2cb0e0
Azis202017 <azisa6980@gmail.com>
2024-01-01 15:36:06 +0700

    [Theme] added theme into application

--- lib/app/shared/theme/theme.dart
+++ lib/app/shared/theme/theme.dart
@@ -14,6 +14,7 @@ ThemeData themeData = ThemeData(
         borderRadius: BorderRadius.circular(40),
       ),
       disabledBackgroundColor: neutralDisable,
+      disabledForegroundColor: neutralLight,
     ),
   ),
   textButtonTheme: TextButtonThemeData(
