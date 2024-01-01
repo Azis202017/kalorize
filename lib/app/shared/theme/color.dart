3966ef7c3a359a3315707aff70f591b079f0c890
Azis202017 <azisa6980@gmail.com>
2024-01-01 15:35:13 +0700

    [Theme] added color design system

--- lib/app/shared/theme/color.dart
+++ lib/app/shared/theme/color.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
-Color error = const Color(0xffD00000);
 
+const Color mainPrimary = Color(0xffF94917);
+const Color mainSecondary = Color(0xff413D66);
+const Color backgroundPrimary = Color(0xffFEEDE8);
+const Color backgroundSecondary = Color(0xffFEEDE8);
+const Color errorPrimary = Color(0xffFF4644);
+const Color errorSecondary = Color(0xffFFECEC);
+const Color successPrimary = Color(0xff1AB269);
+const Color successSecondary = Color(0xffE9FCF2);
+const Color neutralPrimary = Color(0xff2B293D);
+const Color neutralSecondary = Color(0xff4D555C);
+const Color neutralDisable = Color(0xff757D85);
+const Color neutralLine = Color(0xffD6DCE2);
 
-Color primaryColor = const Color(0xffF94917);
-
-Color whiteColor = const Color(0xffFFFFFF);
-Color inputColor = const Color(0xffEBEEF0);
-Color font = const Color(0xff0D0D13);
+const Color neutralLight = Color(0xffF8F9FA);
+const Color neutralForm = Color(0xffEBEEF0);
 
