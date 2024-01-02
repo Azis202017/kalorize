c25c5b9fa8fbda2737d67890abab695baae21f59
Azis202017 <azisa6980@gmail.com>
2024-01-03 03:15:03 +0700

    [Change Profile] added validation and change state of the button

--- lib/app/modules/change_profile/controllers/change_profile_controller.dart
+++ lib/app/modules/change_profile/controllers/change_profile_controller.dart
@@ -2,25 +2,81 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ChangeProfileController extends GetxController {
-  final formKey = GlobalKey<FormState>;
+  TextEditingController namaEditingController = TextEditingController();
+  TextEditingController emailEditingController = TextEditingController();
+  TextEditingController noTeleponController = TextEditingController();
+
+  final GlobalKey formKey = GlobalKey<FormState>();
   FocusNode namaFocusNode = FocusNode();
   FocusNode emailFocusNode = FocusNode();
   FocusNode noTeleponFocusNode = FocusNode();
 
-  String? nama = "";
-  String? email = "";
-  String? noTelepon = "";
+  String nama = "";
+  String email = "";
+  String noTelepon = "";
 
-  String? onChangeNama(String? value) => nama = value;
-  String? onChangeEmail(String? value) => email = value;
-  String? onChangeNomorTelepon(String? value) => noTelepon = value;
+  String? onChangeNama(String? value) {
+    nama = value!;
+    update();
+    return nama;
+  }
+
+  String? onChangeEmail(String? value) {
+    email = value!;
+
+    update();
+    return email;
+
+  }
+
+  String? onChangeNomorTelepon(String? value) {
+    noTelepon = value!;
+    update();
+    return noTelepon;
+  }
+
+  bool isButtonSaveActive = false;
 
   void onSubmittedNama() {
     FocusScope.of(Get.context!).requestFocus(emailFocusNode);
   }
+
   void onSubmittedEmail() {
     FocusScope.of(Get.context!).requestFocus(noTeleponFocusNode);
   }
-  
-  
+
+  String? onValidateNama(String? value) {
+    if (value!.isEmpty) {
+      return "Mohon isi nama nya terlebih dahulu";
+    }
+    return null;
+  }
+
+  String? onValidateEmail(String? value) {
+    if (value!.isEmpty) {
+      return "Mohon isi nama nya terlebih dahulu";
+    } else if (!value.contains("@")) {
+      return "Tambahkan @ untuk email yang benar";
+    }
+    return null;
+  }
+
+  String? onValidateNomorTelepon(String? value) {
+    if (value!.isEmpty) {
+      return "Mohon isi nomor telepon nya terlebih dahulu";
+    }
+    return null;
+  }
+
+  bool changeButtonActive() {
+    isButtonSaveActive = nama.isNotEmpty &&
+        email.isNotEmpty &&
+        email.contains("@") &&
+        noTelepon.isNotEmpty;
+    print(isButtonSaveActive);
+    update();
+    return isButtonSaveActive;
+  }
+
+  void changeProfile() {}
 }
--- lib/app/modules/change_profile/views/change_profile_view.dart
+++ lib/app/modules/change_profile/views/change_profile_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:kalorize/app/shared/widgets/headers_with_button.dart';
+import 'package:kalorize/app/shared/widgets/text_input.dart';
 
 import '../controllers/change_profile_controller.dart';
 
@@ -9,16 +10,64 @@ class ChangeProfileView extends GetView<ChangeProfileController> {
   const ChangeProfileView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: HeaderWithButton(
-        
-      ),
-      body: const Center(
-        child: Text(
-          'ChangeProfileView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+    return GetBuilder<ChangeProfileController>(
+      builder: (_) {
+        return Form(
+          key: controller.formKey,
+          child: Scaffold(
+            appBar:  HeaderWithButton(
+              onTap: controller.changeButtonActive() ? controller.changeProfile : null,
+            ),
+            body: SingleChildScrollView(
+              child: Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 25,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextInput(
+                      textInputType: TextInputType.name,
+                      title: 'NamaLengkap',
+                      textEditingController: controller.namaEditingController,
+                      validator: controller.onValidateNama,
+                      onEditingComplete: controller.onSubmittedNama,
+                      onChange: controller.onChangeNama,
+
+                      hintText: 'John doe',
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextInput(
+                      title: 'Email',
+                      textInputType: TextInputType.emailAddress,
+                      validator: controller.onValidateEmail,
+                      textEditingController: controller.emailEditingController,
+                      onEditingComplete: controller.onSubmittedEmail,
+                      onChange: controller.onChangeEmail,
+
+                      hintText: 'user@gmail.com',
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextInput(
+                      textInputType: TextInputType.number,
+                      title: 'Nomor Telepon',
+                      textEditingController: controller.noTeleponController,
+                      hintText: '082211222',
+                      validator: controller.onValidateNomorTelepon,
+                      onChange: controller.onChangeNomorTelepon,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      }
     );
   }
 }
