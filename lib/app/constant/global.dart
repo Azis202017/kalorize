44caf998849d56b6ea770df0edbe46c3e5f6b9c7
Azis202017 <azisa6980@gmail.com>
2024-01-15 00:14:21 +0700

    change api url and also storage

--- lib/app/constant/global.dart
+++ lib/app/constant/global.dart
@@ -1,5 +1,8 @@
-const apiUrl = "https://06f1-36-71-84-82.ngrok-free.app/api/v1";
-const Map<String,String> headers = {
-  "Content-Type" : "application/json",
-  'Accept' : 'application/json',
+import 'package:get_storage/get_storage.dart';
+
+const apiUrl = "https://d3f4-36-71-81-80.ngrok-free.app/api/v1";
+const Map<String, String> headers = {
+  "Content-Type": "application/json",
+  'Accept': 'application/json',
 };
+GetStorage storage = GetStorage();
