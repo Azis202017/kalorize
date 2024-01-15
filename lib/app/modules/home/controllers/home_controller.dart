2ff000f616a7546ee5cfca2f7e27a352ea8bc98a
Azis202017 <azisa6980@gmail.com>
2024-01-16 04:27:44 +0700

    added date time or calendar

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:kalorize/app/helpers/date_time_format.dart';
 
 class HomeController extends GetxController {
-  DateTime selectedDate = DateTime.now();
+  TextEditingController dateController = TextEditingController();
+  DateTime? selectedDate;
+  @override
+  void onInit() {
+    super.onInit();
+
+    dateController.text = 'Hari ini ${dateFormat(selectedDate)}';
+  }
 
   Future<void> showDate() async {
     DateTime currentDate = DateTime.now();
 
+    await initializeDateFormatting(); // Initialize date formatting
+
     DateTime? pickedDate = await showDatePicker(
       context: Get.context!,
-      initialDate: selectedDate,
-      firstDate: DateTime(1900), // Batasan hingga tanggal awal yang diinginkan
-      lastDate: currentDate,     // Batasan hingga tanggal hari ini
+      initialDate: selectedDate ?? currentDate,
+      firstDate: DateTime(1900),
+      lastDate: currentDate,
     );
 
-    if (pickedDate != null && pickedDate != selectedDate) {
+    if (pickedDate != null) {
       selectedDate = pickedDate;
+
+      String formattedPickedDate = DateFormat.yMd().format(pickedDate);
+      String formattedCurrentDate = DateFormat.yMd().format(currentDate);
+
+      if (formattedPickedDate == formattedCurrentDate) {
+        dateController.text = 'Hari ini ${dateFormat(selectedDate)}';
+      } else {
+        dateController.text = dateFormat(selectedDate);
+      }
+
+      print(formattedPickedDate == formattedCurrentDate);
+
       update();
     }
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 import 'app/routes/app_pages.dart';
 import 'app/shared/theme/theme.dart';
@@ -16,12 +17,14 @@ void main() async{
 
 
   ]);
-  runApp(
-    GetMaterialApp(
-      title: "Kalorize",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-      theme: themeData,
+  await initializeDateFormatting('id_ID', null).then(
+    (_) => runApp(
+      GetMaterialApp(
+        title: "Kalorize",
+        initialRoute: AppPages.INITIAL,
+        theme: themeData,
+        getPages: AppPages.routes,
+      ),
     ),
   );
 }
