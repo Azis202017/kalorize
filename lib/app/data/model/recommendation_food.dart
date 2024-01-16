8dcd46ad31cdb306414ec196e262d99837ffded4
Azis202017 <azisa6980@gmail.com>
2024-01-16 16:15:47 +0700

    added recommended food

--- lib/app/data/model/recommendation_food.dart
+++ lib/app/data/model/recommendation_food.dart
@@ -1,3 +1,6 @@
+// To parse this JSON data, do
+//
+//     final recommendationFood = recommendationFoodFromJson(jsonString);
 
 import 'dart:convert';
 
@@ -7,12 +10,14 @@ String recommendationFoodToJson(RecommendationFood data) => json.encode(data.toJ
 
 class RecommendationFood {
     List<Breakfast>? breakfast;
+    List<Breakfast>? launch;
     List<Breakfast>? dinner;
     int? totalCalories;
     int? totalProtein;
 
     RecommendationFood({
         this.breakfast,
+        this.launch,
         this.dinner,
         this.totalCalories,
         this.totalProtein,
@@ -20,6 +25,7 @@ class RecommendationFood {
 
     factory RecommendationFood.fromJson(Map<String, dynamic> json) => RecommendationFood(
         breakfast: json["Breakfast"] == null ? [] : List<Breakfast>.from(json["Breakfast"]!.map((x) => Breakfast.fromJson(x))),
+        launch: json["Launch"] == null ? [] : List<Breakfast>.from(json["Launch"]!.map((x) => Breakfast.fromJson(x))),
         dinner: json["Dinner"] == null ? [] : List<Breakfast>.from(json["Dinner"]!.map((x) => Breakfast.fromJson(x))),
         totalCalories: json["total_calories"],
         totalProtein: json["total_protein"],
@@ -27,6 +33,7 @@ class RecommendationFood {
 
     Map<String, dynamic> toJson() => {
         "Breakfast": breakfast == null ? [] : List<dynamic>.from(breakfast!.map((x) => x.toJson())),
+        "Launch": launch == null ? [] : List<dynamic>.from(launch!.map((x) => x.toJson())),
         "Dinner": dinner == null ? [] : List<dynamic>.from(dinner!.map((x) => x.toJson())),
         "total_calories": totalCalories,
         "total_protein": totalProtein,
