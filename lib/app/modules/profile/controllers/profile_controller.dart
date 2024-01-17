2611a47bcc14d79829b17b0bb51809b93319a14d
Azis202017 <azisa6980@gmail.com>
2024-01-17 23:31:52 +0700

    profile controller and profile view

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -1,25 +1,50 @@
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:kalorize/app/data/model/user_model.dart';
 import 'package:kalorize/app/helpers/alert_confirm.dart';
+import 'package:kalorize/app/routes/app_pages.dart';
+import 'package:kalorize/app/services/users_service.dart';
+
+import '../../../constant/global.dart';
 
 class ProfileController extends GetxController {
-  
-
+  UserModel? user = UserModel();
+  XFile? image;
   final count = 0.obs;
 
+  bool isLoading = true;
 
+  @override
+  void onInit() {
+    getUserData();
+    super.onInit();
+  }
 
   void increment() => count.value++;
   void confirmLogout() {
     alertConfirm(
-      title: 'Apakah kamu yakin ingin keluar?',
-      saveTitle: "Tetep Masuk",
-      saveButtonTap: () => Get.back(),
-      cancelTitle: 'Keluar',
-      cancelButtonTap: logout
-
-    );
+        title: 'Apakah kamu yakin ingin keluar?',
+        saveTitle: "Tetep Masuk",
+        saveButtonTap: () => Get.back(),
+        cancelTitle: 'Keluar',
+        cancelButtonTap: logout);
   }
-  void logout() {
 
+  void logout() {
+    Get.offAllNamed(Routes.LOGIN);
+    storage.remove('token');
+  }
+
+  void getUserData() async {
+    user = await UserService().fetchUserData();
+    isLoading = false;
+
+    update();
+  }
+
+  void getFoto() async {
+    image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    
+    update();
   }
 }
