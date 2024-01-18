2d7e27245eb67936cefc7ea1098b654557f0fa9b
Azis202017 <azisa6980@gmail.com>
2024-01-18 23:07:43 +0700

    added new url global

--- lib/app/constant/global.dart
+++ lib/app/constant/global.dart
@@ -2,6 +2,7 @@ import 'package:get_storage/get_storage.dart';
 
 const apiUrl = "https://kalorize-api.fly.dev/api/v1";
 const mlUrl = "https://kalorize-ghleyl575q-uc.a.run.app/get-recommendation";
+const mlUrlTF = "https://kalorize-ml-idn-tf-ghleyl575q-uc.a.run.app/get-recommendation";
 const Map<String, String> headers = {
   "Content-Type": "application/json",
   'Accept': 'application/json',
