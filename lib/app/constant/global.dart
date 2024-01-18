84ca399012f151f710477dbe8daaa497fcce3a11
Azis202017 <azisa6980@gmail.com>
2024-01-18 15:55:56 +0700

    delete home and also global

--- lib/app/constant/global.dart
+++ lib/app/constant/global.dart
@@ -1,7 +1,7 @@
 import 'package:get_storage/get_storage.dart';
 
 const apiUrl = "https://kalorize-api.fly.dev/api/v1";
-const mlUrl = "https://44cd-202-67-40-210.ngrok-free.app/get-recommendation";
+const mlUrl = "https://kalorize-ghleyl575q-uc.a.run.app/get-recommendation";
 const Map<String, String> headers = {
   "Content-Type": "application/json",
   'Accept': 'application/json',
